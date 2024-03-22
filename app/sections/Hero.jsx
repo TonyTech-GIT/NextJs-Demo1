875d3a7bcc6b81268cb1c4bbe64a58754ca2e021
TonyTech-GIT <toomgbebu@gmail.com>
2024-03-22 22:48:19 +0100

    progress on the hero section

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -1,9 +1,31 @@
+import Image from "next/image"
+import bigImage from "../../public/assets/big-img.png"
+
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Hero section</h1>
-        </div>
+        <section id="hero" className="mt-[6rem] w-full max-container flex lg:flex-row flex-col justify-center min-h-screen max-md:gap-4">
+
+            <div className="relative flex flex-1 flex-col justify-center items-center text-white bg-black rounded-sm lg:w-full ">
+
+                <h1 className="lg:text-4xl leading-normal font-plus-jakata font-bold">Lorem, ipsum <span className="text-yellow-400">dolor.</span>
+                    <br />
+                    Lorem ipsum dolor sit.
+                    <br />
+                    <span className="text-yellow-400">Lorem, ipsum.</span>
+
+                </h1>
+
+            </div>
+
+            <div className="relative flex flex-1 bg-[#e5e5e5] bg-center justify-center items-center w-full">
+                <Image
+                    src={bigImage}
+                    alt="big image"
+                    className="object-cover shadow-xl rounded-lg"
+                />
+            </div>
+        </section>
     )
 }
 
