acf0adf563150e9ae6dea0310fc7af71c0d5f686
TonyTech-GIT <toomgbebu@gmail.com>
2024-03-23 20:21:50 +0100

    progress on the about section

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,9 +1,37 @@
 // import {menuBtn, menuClose} from '../../public/assets/'
 
+import fastIcon from '../../public/assets/icons/fast.png'
+import deliveryIcon from '../../public/assets/icons/delivery.png'
+import onTimeIcon from '../../public/assets/icons/on-time.png'
+import securityIcon from '../../public/assets/icons/security.png'
 
 export const navLinks = [
     { href: "#home", label: "Home" },
     { href: "#hero", label: "Hero" },
     { href: "#about", label: "About" },
     { href: "#footer", label: "Footer" }
+]
+
+export const cardsInfo = [
+
+    {
+        imgUrl: fastIcon,
+        label: "Fast",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eum similique quidem quis possimus quas."
+    },
+    {
+        imgUrl: deliveryIcon,
+        label: "Efficient Delivery",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eum similique quidem quis possimus quas."
+    },
+    {
+        imgUrl: onTimeIcon,
+        label: "On-Time Services",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eum similique quidem quis possimus quas."
+    },
+    {
+        imgUrl: securityIcon,
+        label: "Secure",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat eum similique quidem quis possimus quas."
+    }
 ]
--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -1,9 +1,19 @@
 
 const About = () => {
     return (
-        <div>
-            <h1>About section</h1>
-        </div>
+        <section className="max-container flex flex-col justify-center">
+            <div className=" py-12 bg-gradient-to-r from-black to-yellow-400 flex self-center justify-center shadow-2xl rounded-xl lg:w-[65%]">
+
+                <h1 className="lg:text-3xl text-center text-white leading-normal">Lorem ipsum
+                    <span className="text-yellow-400"> dolor </span>
+                    sit amet.
+                </h1>
+            </div>
+
+            <div>
+
+            </div>
+        </section>
     )
 }
 
